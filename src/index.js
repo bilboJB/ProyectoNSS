ab9d22cc31b1b860fd7afa113cfc04665fa5e915
bilboJB <computersjavier@gmail.com>
2024-03-12 18:53:55 -0400

    agregado cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const taskRoutes = require('./rutas/users.rutas')
 
 const app = express();
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(taskRoutes);
